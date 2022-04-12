fa1881436f3df14efae14761ce3e24fb86d9783b
AleksandrKoryakin <88359979+AleksandrKoryakin@users.noreply.github.com>
2022-04-12 09:53:33 +0300

    Final version.
    Need to add stopping video background playback

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const modalWindow = document.querySelector('.modal');
+const buttonsModal = document.querySelectorAll('.button-play');
+const iframe = document.querySelector('.iframe');
+
+buttonsModal.forEach((item, i) => {
+  item.addEventListener('click', () => {
+    modalWindow.classList.add('active');
+  })
+})
+
+modalWindow.addEventListener('click', () => {
+  modalWindow.classList.remove('active');
+})
